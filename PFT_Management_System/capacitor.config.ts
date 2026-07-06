ffccd78b7a9773fd8adead31e036a5f18b4eb003
Ruchit Pitaliya <ruchit.pitaliya@corpnet.co.in>
2026-07-06 16:03:14 +0530

    test apk genration

---
+++ PFT_Management_System/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.company.finance',
+  appName: 'Finance Tracker',
+  webDir: 'dist/PFT_Management_System/browser'
+};
+
+export default config;
